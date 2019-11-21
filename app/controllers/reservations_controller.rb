f203fd22d8e77eae91de46872cf2f7984d51d391
Chris Kerr <chriskerr@me.com>
2019-11-21 11:09:48 +1100

    fixed delete

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -79,6 +79,7 @@ class ReservationsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
