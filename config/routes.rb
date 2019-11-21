0eb53b1fb02b41c49cf4783f687a0aaee739d890
Chris Kerr <chriskerr@me.com>
2019-11-21 12:13:59 +1100

    added destinations API /:city/destinations outputs an array of possible destinations

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   resources :flights
   resources :users, only: [:index, :show, :new, :create]
   resources :planes
-
+  
   get '/cities' => 'flights#cities'
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/' => 'session#destroy'
+  get '/:origin/destinations' => 'flights#destinations'
   get '/:origin/:destination' => 'flights#search'
   get '/:origin' => 'flights#search'
 end
